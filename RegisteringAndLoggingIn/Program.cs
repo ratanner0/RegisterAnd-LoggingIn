2215ea976b6b1efaeb1466b971e59e4633361b1b
Richard Tanner <ratanner0@gmail.com>
2022-07-09 18:49:44 -0400

    Add files via upload

---
+++ RegisteringAndLoggingIn/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace RegisteringAndLoggingIn
+{
+    internal class Program
+    {
+        static string username;
+        static string password;
+
+        static void Main(string[] args)
+        {
+            Register();
+            Login();
+            Console.Read();
+            
+        }
+        public static void Register()
+        {
+            Console.WriteLine("Please enter your username");
+            username = Console.ReadLine();
+            Console.WriteLine("Please enter your password");
+            password = Console.ReadLine();
+            Console.WriteLine("Registration Completed");
+            Console.WriteLine("-----------------------------------------------");
+        }
+        public static void Login()
+        {
+            Console.WriteLine("Please enter your username:");
+            if (username == Console.ReadLine())
+            {
+                Console.WriteLine("Please enter your password:");
+                if (password == Console.ReadLine())
+                {
+                    Console.WriteLine("Login successful");
+                }
+                else
+                {
+                    Console.WriteLine("Login Failed, wrong password.  Restart Program");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Login Failed, wrong username.  Restart Program");
+            }
+        }
+    }
+}
